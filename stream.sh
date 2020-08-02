d8c85ca61ba3438ca966c75f88de29f313a491bd
Rufo Sanchez <rufo@rufosanchez.com>
2020-08-02 10:44:12 -0500

    attempt to use PORT env var

--- stream.sh
+++ stream.sh
@@ -3,5 +3,7 @@
 trap "exit" INT TERM ERR
 trap "kill 0" EXIT
 
+./build.ffserver.conf.sh
+
 ffserver -hide_banner -loglevel ${FFSERVER_LOG_LEVEL} &
-ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:8090/feed.ffm
+ffmpeg -hide_banner -loglevel ${FFMPEG_LOG_LEVEL} -rtsp_transport tcp ${FFMPEG_INPUT_OPTS} -i ${RTSP_URL} ${FFMPEG_OUTPUT_OPTS} http://127.0.0.1:$PORT/feed.ffm
